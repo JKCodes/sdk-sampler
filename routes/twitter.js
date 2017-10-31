6850a5499a63c66ab4ec70610dfbc42717ced4f2
Joe Kim <josephkim0224@gmail.com>
2017-10-31 13:34:51 -0400

    refactor twitter.js

--- routes/twitter.js
+++ routes/twitter.js
@@ -2,43 +2,64 @@ var express = require('express')
 var router = express.Router()
 var Twitter = require('twitter')
 
-var client = new Twitter({
+function getClient() {
+  var client = new Twitter({
     consumer_key: 'JgC4F0tlzAAHA14Is7hjFccJa',
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: '54285575-g7DJmwkxRIKmJHpAVlxsypqhwbYZykPNzYtBTWmNI',
     access_token_secret: process.env.TWITTER_ACCESS_SECRET
   })
 
-router.get('/', function(req, res, next) {
+  return client
+}
+
+router.get('/:action', function(req, res, next) {
   
-  var username = req.query.username
-  if (!username) {
-    var search = req.query.search
-    if (!search) {
+  var action = req.params.action
+  if (action == 'timeline') {
+    var username = req.query.username
+    var client = getClient()
+
+    if (!username) {
       res.json({
         confirmation: 'fail',
-        message: 'No search term or username detected'
+        message: 'No username detected'
       })
 
       return
     }
 
-    client.get('search/tweets', {q: search}, function(error, tweets, response) {
+    var params = { screen_name: username}
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets)
+      }
+
+      res.json(tweets)
+
+    })
+  }
+
+  if (action == 'search') {
+    var term = req.query.term
+    var client = getClient()
+
+    if (!term) {
+      res.json({
+        confirmation: 'fail',
+        message: 'No term detected'
+      })
+
+      return
+    }
+
+    client.get('search/tweets', {q: term}, function(error, tweets, response) {
       res.json(tweets)
     })
 
     return
   }
-
-  var params = { screen_name: username}
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets)
-    }
-
-    res.json(tweets)
-
-  })
+  
 })
 
 module.exports = router
