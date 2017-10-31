fb9c2790e2af0067234b226095480e40425303e1
Joe Kim <josephkim0224@gmail.com>
2017-10-31 14:28:47 -0400

    create search route for yelp

--- routes/yelp.js
+++ routes/yelp.js
@@ -1,10 +1,39 @@
 var express = require('express')
 var router = express.Router()
+var Yelp = require('yelp-api-v3')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({
-    confirmation: 'hi'
+router.get('/:action', function(req, res, next) {
+  
+  var actions = ['search']
+  var action = req.params.action
+
+  if (actions.indexOf(action) == -1) {
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid resource'
+    })
+  }
+
+  var yelp = new Yelp({
+    app_id: process.env.YELP_APP_ID,
+    app_secret: process.env.YELP_APP_SECRET
+  })
+
+  var params = { term: req.query.term, location: req.query.location }
+
+  yelp.search(params)
+  .then(function(data) {
+    res.json({
+      confirmation: 'success',
+      data: JSON.parse(data)
+    })
+  })
+  .catch(function(err) {
+    res.json({
+      confirmation: 'fail',
+      message: err
+    })
   })
 })
 
