ad3c8453cc307eab87a1bdaa5136e66fdf7911cb
Joe Kim <josephkim0224@gmail.com>
2017-10-31 13:19:22 -0400

    dynamic username query

--- routes/twitter.js
+++ routes/twitter.js
@@ -2,16 +2,29 @@ var express = require('express')
 var router = express.Router()
 var Twitter = require('twitter')
 
-router.get('/', function(req, res, next) {
-  
-  var client = new Twitter({
+var client = new Twitter({
     consumer_key: 'JgC4F0tlzAAHA14Is7hjFccJa',
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: '54285575-g7DJmwkxRIKmJHpAVlxsypqhwbYZykPNzYtBTWmNI',
     access_token_secret: process.env.TWITTER_ACCESS_SECRET
   })
 
-  var params = { screen_name: 'nodejs'}
+router.get('/', function(req, res, next) {
+  
+  var username = req.query.username
+  if (!username) {
+    var search = req.query.search
+    if (!search) {
+      res.json({
+        confirmation: 'fail',
+        message: 'No search term or username detected'
+      })
+
+      return
+    }
+  }
+
+  var params = { screen_name: username}
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
       console.log(tweets)
