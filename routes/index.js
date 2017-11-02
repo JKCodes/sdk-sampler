fd32a28bc8da4594cec6287cd7c84530383cd1dc
Joe Kim <josephkim0224@gmail.com>
2017-11-02 15:23:49 -0400

    form submit now supports both searching and timeline

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,14 @@ router.post('/:page', function(req, res, next) {
   var query = req.body.query
   var type = req.body.type
 
-  var url = '/twitter/search?term='+ query
-  res.redirect(url)
+  var url = null
 
+  if (type == 'search')
+    url = '/twitter/search?term=' + query
+  if (type == 'timeline')
+    url = '/twitter/timeline?username=' + query
+
+  res.redirect(url)
 })
 
 module.exports = router
